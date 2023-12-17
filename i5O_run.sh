f68ddaf01dec1820c6ffe1018048d52bbd7e8ecf
fibrahim30 <fibrahim30@gatech.edu>
2023-12-17 16:53:42 -0500

    Edit bash script

--- i5O_run.sh
+++ i5O_run.sh
@@ -14,41 +14,51 @@ for ARGUMENT in "$@"; do
 	export "$KEY"="$VALUE"
 done
 
-i5O_videos="/data/i5O/i5OData/undercover-left/videos/20220412/"
+# Specify the root directory
+root_dir="/data/i5O/i5OData/undercover-left/videos/"
 
-echo $i5O_videos
+# Iterate through each subdirectory that contains .mp4 files
+for dir in "$root_dir"*/; do
+    if [ -d "$dir" ] && ls -1 "$dir"/*.mp4 &>/dev/null; then
+        # Process .mp4 files in the current directory
+        echo "Processing files in: $dir"
+        IFS=$'\n'
+        set -f
 
-# -------------------------------------------------
+        # build lst
+        lst="["
 
-# build lst
-lst="["
+        count=0
 
-count=0
-for vid in $(find "$i5O_videos" -name "*.mp4"); do
+        for vid in "$dir"*.mp4; do
+            # ensure that there is at least 20GB of data left
+            if [ $(expr $(df -B1 /data/ | awk 'NR==2 {print $4}') / 1000000000) -gt 10 ]; then
 
-  # ensure that there is at least 20GB of data left
-  if [ $(expr $(df -B1 /data/ | awk 'NR==2 {print $4}') / 1000000000) -gt 10 ]; then
+            # unset IFS
+            # set +f
+            echo $vid
+            if [[ $count -ne 0 ]]; then
+              lst="$lst, $vid"
+            else
+              lst="${lst}${vid}"
+            fi
 
-	# unset IFS
-	# set +f
-  echo $vid
-  if [[ $count -ne 0 ]]; then
-    lst="$lst, $vid"
-  else
-    lst="${lst}${vid}"
-  fi
+            count=$(expr $count + 1)
 
-  count=$(expr $count + 1)
+            IFS=$'\n'
+            set -f
 
-  IFS=$'\n'
-	set -f
+            fi
+        done
 
-  fi
-
+        # Reset IFS and set +f if needed for subsequent operations
+        unset IFS
+        set +f
+    fi
 done
 
 lst="$lst]"
-# echo $lst
+echo $lst
 
 # run the script
 # python ./main.py \
@@ -62,3 +72,47 @@ lst="$lst]"
 
 unset IFS
 set +f
+
+# -------------------------------------------------
+
+# # build lst
+# lst="["
+
+# count=0
+# for vid in $(find "$i5O_videos" -name "*.mp4"); do
+
+#   # ensure that there is at least 20GB of data left
+#   if [ $(expr $(df -B1 /data/ | awk 'NR==2 {print $4}') / 1000000000) -gt 10 ]; then
+
+# 	# unset IFS
+# 	# set +f
+#   echo $vid
+#   if [[ $count -ne 0 ]]; then
+#     lst="$lst, $vid"
+#   else
+#     lst="${lst}${vid}"
+#   fi
+
+#   count=$(expr $count + 1)
+
+#   IFS=$'\n'
+# 	set -f
+
+#   fi
+
+# done
+
+# lst="$lst]"
+# echo $lst
+
+# # run the script
+# python ./main.py \
+#   feature_type=i3d \
+#   device="cuda:0" \
+#   video_paths=$lst \
+#   output_path="/data/fady/aar/TemporalMaxer/data/i5O/features_i3d/ \
+#   on_extraction="save_numpy"
+
+
+# unset IFS
+# set +f
