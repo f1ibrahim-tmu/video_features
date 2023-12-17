fceeaeee247f1cf25a0e62f90ca4f02ac0326b8f
fibrahim30 <fibrahim30@gatech.edu>
2023-12-17 16:39:52 -0500

    fixed script

--- i5O_run.sh
+++ i5O_run.sh
@@ -14,7 +14,7 @@ for ARGUMENT in "$@"; do
 	export "$KEY"="$VALUE"
 done
 
-i5O_videos="/data/i5O/i5OData/undercover-right/videos/20220412/"
+i5O_videos="/data/i5O/i5OData/undercover-left/videos/20220412/"
 echo $i5O_videos
 
 # -------------------------------------------------
