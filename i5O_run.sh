bad1edd010cc94157a58550875194b4853f10a22
fibrahim30 <fibrahim30@gatech.edu>
2023-12-17 20:13:05 -0500

    Fixed the bad variable name error

--- i5O_run.sh
+++ i5O_run.sh
@@ -28,7 +28,7 @@ if [ -d "$videos_dir" ]; then
         echo "Folder: $folder"
         echo "Files in $folder:"
 
-        find "$folder" -maxdepth 1 -type f -name "*.mp4" -print0 | while read -r '' file; do
+        find "$folder" -maxdepth 1 -type f -name "*.mp4" -print0 | while read -r file; do
             result+="Folder: $folder File: $file"$'\n'
         done
 
