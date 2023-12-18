1e0a1d20ae6ae3c8ae0565fcbd2fa03e254855d1
fibrahim30 <fibrahim30@gatech.edu>
2023-12-17 23:20:30 -0500

    fixed missing quote

--- i5O_run.sh
+++ i5O_run.sh
@@ -41,5 +41,69 @@ python ./main.py \
   feature_type=i3d \
   device="cuda:0" \
   video_paths=$lst \
-  output_path="/data/fady/aar/TemporalMaxer/data/i5O/features_i3d/ \
-  on_extraction="save_numpy"
+  output_path="/data/fady/aar/TemporalMaxer/data/i5O/features_i3d/" \
+  on_extraction="save_numpy" 
+
+
+# -------------------------------------------------------------------------------------------------
+
+# # Check if the directory exists
+# if [ -d "$videos_dir" ]; then
+#     # Iterate through directories found by find and list files
+#     find "$videos_dir" -mindepth 1 -maxdepth 1 -type d | while read -r folder; do
+#         # echo "Folder: $folder"
+#         # echo "Files in $folder:"
+#         # ls -p "$folder" | grep -v /
+
+#         find "$folder" -maxdepth 1 -type f -name "*.mp4" | while read -r file; do
+#             echo $file
+#         done
+#     done
+# else
+#     echo "Directory $videos_dir does not exist."
+# fi
+
+
+# -------------------------------------------------
+
+# # build lst
+# lst="["
+
+# count=0
+# for vid in $(find "$i5O_videos" -name "*.mp4"); do
+
+#   # ensure that there is at least 20GB of data left
+#   if [ $(expr $(df -B1 /data/ | awk 'NR==2 {print $4}') / 1000000000) -gt 10 ]; then
+
+# 	# unset IFS
+# 	# set +f
+#   echo $vid
+#   if [[ $count -ne 0 ]]; then
+#     lst="$lst, $vid"
+#   else
+#     lst="${lst}${vid}"
+#   fi
+
+#   count=$(expr $count + 1)
+
+#   IFS=$'\n'
+# 	set -f
+
+#   fi
+
+# done
+
+# lst="$lst]"
+# echo $lst
+
+# # run the script
+# python ./main.py \
+#   feature_type=i3d \
+#   device="cuda:0" \
+#   video_paths=$lst \
+#   output_path="/data/fady/aar/TemporalMaxer/data/i5O/features_i3d/ \
+#   on_extraction="save_numpy"
+
+
+# unset IFS
+# set +f
